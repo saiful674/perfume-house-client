8974da56bbfecb93e90481668508e0f7c9efecc9
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2024-07-16 21:36:25 +0600

    product data and best selling products section added

--- src/pages/homePage/home/Home.tsx
+++ src/pages/homePage/home/Home.tsx
@@ -1,4 +1,5 @@
 import Banner from "@/UI/components/banner/Banner";
+import BestSelling from "../bestSelling/BestSelling";
 import Category from "../category/Category";
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <div>
       <Banner />
       <Category />
+      <BestSelling />
     </div>
   );
 };
